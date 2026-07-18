ed409a51ed6d34c410d0631efc7ace8f7413be5b
Chimoneg27 <chimonegarvin27@gmail.com>
2026-07-18 12:40:48 +0200

    feat(service): create the graphql service for the pokedex

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,28 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, inject } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { provideApollo } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
+import { InMemoryCache } from '@apollo/client';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes),
     provideClientHydration(),
+    provideHttpClient(),
+    provideApollo(() => {
+      const httpLink = inject(HttpLink);
+
+      return {
+        link: httpLink.create({
+          uri: 'https://beta.pokeapi.co/graphql/v1beta',
+        }),
+        cache: new InMemoryCache(),
+      };
+    }),
   ],
 };
