8a6aa71290fe5168f7e6554428d8da1f4d5fe83a
Chimoneg27 <chimonegarvin27@gmail.com>
2026-07-18 12:41:39 +0200

    refactor(app): display pokedex content from api

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
+import { Pokedex } from './pokedex/pokedex';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, Pokedex],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
