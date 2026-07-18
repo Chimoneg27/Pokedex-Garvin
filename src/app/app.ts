95f0cf6ccefd6b444f22297871c87a8841951f8d
Chimoneg27 <chimonegarvin27@gmail.com>
2026-07-18 13:39:18 +0200

    refactor(models): move the pokedex interface into models

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./components/header/header";
-import { Pokedex } from './pokedex/pokedex';
+import { Pokedex } from './products/pokedex/pokedex';
 
 @Component({
   selector: 'app-root',
---
+++ src/app/services/models/pokemon-row.ts
@@ -0,0 +1,12 @@
+export interface PokemonRow {
+  name: string;
+  sprite: string;
+  types: string;
+  hp: number;
+  attack: number;
+  spAtk: number;
+  spDef: number;
+  speed: number;
+  total: number;
+  typeColor: string;
+}
