a56cc6db9e26350a3494c9a7e4a0d7a0a9d2ebe2
Chimoneg27 <chimonegarvin27@gmail.com>
2026-07-19 13:55:40 +0200

    refactor(products): create store for teams

--- src/app/products/teams/teams.ts
+++ src/app/products/teams/teams.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TeamsStore } from './state/team.store';
+import { CREATE_TEAM, GET_TEAMS } from '../../services/graphql/queries';
+import {MatButtonModule} from '@angular/material/button';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { response } from 'express';
 
 @Component({
   selector: 'app-teams',
-  imports: [],
+  imports: [MatProgressSpinner, MatButtonModule, ReactiveFormsModule],
   templateUrl: './teams.html',
   styleUrl: './teams.css',
 })
-export class Teams {}
+
+export class Teams {
+  private http = inject(HttpClient);
+  private store = inject(TeamsStore);
+  private apiUrl = 'http://localhost:4000';
+
+
+  teams$ = toSignal(this.store.teams$, { initialValue: [] })
+  loading$ = toSignal(this.store.loading$, { initialValue: false })
+  error$ = toSignal(this.store.error$, { initialValue: null })
+
+
+  fetchTeams(): void {
+    this.store.setLoading(true);
+
+    this.http.post<any>(this.apiUrl, { query: GET_TEAMS }).subscribe({
+      next: (response) => {
+        if (response.errors) {
+          this.store.setError(response.errors[0].message);
+          return;
+        }
+        this.store.setTeams(response.data.allTeams);
+      },
+      error: (err) => {
+        console.error('HTTP ERROR:', err);
+        this.store.setError('Failed to fetch teams');
+      },
+    });
+  }
+
+
+
+  constructor() {
+    this.fetchTeams();
+  }
+}
